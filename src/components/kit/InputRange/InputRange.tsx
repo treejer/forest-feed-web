c36870fd21b489c2d791c01035e46b6d36ff21a0
armin-bkh <arminbkh0921@yahoo.com>
2023-07-25 14:00:48 +0330

    InputRange component style updated

--- src/components/kit/InputRange/InputRange.tsx
+++ src/components/kit/InputRange/InputRange.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import {useTranslations} from 'use-intl';
 
+import './InputRange.scss';
+
 export type InputRangeProps = {
   value: number;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
@@ -13,12 +15,23 @@ export function InputRange(props: InputRangeProps) {
 
   const t = useTranslations();
 
+  const minPos = ((0 - min) / (max - min)) * 100;
+
+  const maxPos = ((value - min) / (max - min)) * 100;
+
   return (
-    <div className="slideContainer">
-      <input type="range" min={min} max={max} value={value} onChange={onChange} className="slider" id="myRange" />
-      <p>
-        {t('value')} <span id="demo">{value}</span>
-      </p>
+    <div className="wrapper">
+      <div className="input-wrapper">
+        <input className="input" type="range" value={value} min={min} max={max} step={1} onChange={onChange} />
+      </div>
+
+      <div className="control-wrapper">
+        <div style={{left: `0%`}} />
+        <div className="rail overflow-hidden">
+          <div className="inner-rail" style={{left: `${minPos}%`, right: `${100 - maxPos}%`}} />
+        </div>
+        <div className="control" style={{left: `${maxPos}%`}} />
+      </div>
     </div>
   );
 }

37fffa4dc3a76494a5c5f9c1d966ca45d9597c94
armin-bkh <arminbkh0921@yahoo.com>
2023-08-16 22:20:29 +0330

    - Table memo - key issue resolved

--- src/components/kit/Table/Table.tsx
+++ src/components/kit/Table/Table.tsx
@@ -1,16 +1,18 @@
-import React, {useMemo} from 'react';
+import React, {memo, useMemo} from 'react';
 
 import {TableOptions, useSortBy, useTable} from 'react-table';
+
 import {ChevronIcon, ChevronIconDirection} from '@forest-feed/components/kit/Icons/ChevronIcon';
 import {Spacer} from '@forest-feed/components/common/Spacer';
 
 export type TableProps<D extends object> = {
   columns: TableOptions<D>['columns'];
   data: D[];
+  initialState?: TableOptions<D>['initialState'];
 };
 
-export function Table<D extends object>(props: TableProps<D>) {
-  const {columns: originalColumns, data: originalData} = props;
+function TableComponent<D extends object>(props: TableProps<D>) {
+  const {columns: originalColumns, data: originalData, initialState} = props;
 
   const columns = useMemo(() => originalColumns, [originalColumns]);
   const data = useMemo(() => originalData, [originalData]);
@@ -19,14 +21,7 @@ export function Table<D extends object>(props: TableProps<D>) {
     {
       columns,
       data,
-      initialState: {
-        sortBy: [
-          {
-            id: 'createdAt',
-            desc: true,
-          },
-        ],
-      },
+      initialState,
     },
     useSortBy,
   );
@@ -35,46 +30,48 @@ export function Table<D extends object>(props: TableProps<D>) {
     <div className="w-full border border-tableBorder rounded-[12px] overflow-hidden">
       <table {...getTableProps()} className="w-full">
         <thead>
-          {headerGroups.map((headerGroup, index) => (
-            <tr
-              {...headerGroup.getHeaderGroupProps()}
-              key={headerGroup.id}
-              className="border-b border-tableBorder bg-khakiDark"
-            >
-              {headerGroup.headers.map((column, index) => (
-                <th
-                  {...column.getHeaderProps(column.getSortByToggleProps())}
-                  key={column.id}
-                  className="py-2 px-3 font-normal text-sm"
-                >
-                  <div className="flex justify-center items-center">
-                    {column.render('Header')}
-                    {column.isSorted ? (
-                      <span className="flex items-center">
-                        <Spacer />
-                        <ChevronIcon
-                          direction={column.isSortedDesc ? ChevronIconDirection.up : ChevronIconDirection.down}
-                        />
-                      </span>
-                    ) : (
-                      ''
-                    )}
-                  </div>
-                </th>
-              ))}
-            </tr>
-          ))}
+          {headerGroups.map(headerGroup => {
+            const {key, ...props} = headerGroup.getHeaderGroupProps();
+            return (
+              <tr {...props} key={key} className="border-b border-tableBorder bg-khakiDark">
+                {headerGroup.headers.map(column => {
+                  const {key, ...props} = column.getHeaderProps(column.getSortByToggleProps());
+                  return (
+                    <th {...props} key={key} className="py-2 px-3 font-normal text-sm">
+                      <div className="flex justify-center items-center">
+                        {column.render('Header')}
+                        {column.isSorted ? (
+                          <span className="flex items-center">
+                            <Spacer />
+                            <ChevronIcon
+                              direction={column.isSortedDesc ? ChevronIconDirection.up : ChevronIconDirection.down}
+                            />
+                          </span>
+                        ) : (
+                          ''
+                        )}
+                      </div>
+                    </th>
+                  );
+                })}
+              </tr>
+            );
+          })}
         </thead>
         <tbody {...getTableBodyProps()}>
-          {rows.map((row, index) => {
+          {rows.map(row => {
             prepareRow(row);
+            const {key, ...props} = row.getRowProps();
             return (
-              <tr {...row.getRowProps()} key={row.id} className="border-b last:border-none  border-tableBorder flex-1">
-                {row.cells.map((cell, index) => (
-                  <td {...cell.getCellProps()} key={cell.value} className="py-5 px-3 text-center text-sm">
-                    <div className="flex justify-center">{cell.render('Cell')}</div>
-                  </td>
-                ))}
+              <tr {...props} key={key} className="border-b last:border-none border-tableBorder flex-1">
+                {row.cells.map((cell, index) => {
+                  const {key, ...props} = cell.getCellProps();
+                  return (
+                    <td {...props} key={key} className="py-5 px-3 text-center text-sm">
+                      <div className="flex justify-center">{cell.render('Cell')}</div>
+                    </td>
+                  );
+                })}
               </tr>
             );
           })}
@@ -83,3 +80,5 @@ export function Table<D extends object>(props: TableProps<D>) {
     </div>
   );
 }
+
+export const Table = memo(TableComponent) as typeof TableComponent;
